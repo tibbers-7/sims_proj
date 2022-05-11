1298bff6838965e19625c711734a7ba7a2a535b4
Darko Filipovic <darkofilipovic1010@gmail.com>
2022-05-11 17:55:46 +0200

    added orders window and filehandler

---
+++ Zdravo/Zdravo/Model/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zdravo.Model
+{
+    internal class Order
+    {
+        private int id;
+        public int Id => id;
+
+        private string name;
+        public String Name => name;
+
+        private int quantity;
+        public int Quantity => quantity;
+
+        private DateTime orderDateTime;
+        public DateTime OrderDateTime => orderDateTime;
+
+        private string note;
+        public string Note => note;
+
+        public Order(int id,string name,int quantity,DateTime orderDateTime,string note)
+        {
+            this.id = id;
+            this.name = name;
+            this.quantity = quantity;
+            this.orderDateTime = orderDateTime;
+            this.note = note;
+        }
+    }
+}
