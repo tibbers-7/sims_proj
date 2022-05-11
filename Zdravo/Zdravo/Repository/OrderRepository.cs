f7169ac64d9555c4e959a9d6c0640a41f1e5dc7c
Darko Filipovic <darkofilipovic1010@gmail.com>
2022-05-11 18:26:03 +0200

    added orders viewModel

---
+++ Zdravo/Zdravo/Repository/OrderRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using System.Collections.ObjectModel;
+using FileHandler;
+
+namespace Repository
+{
+    public class OrderRepository
+    {
+        private OrderFileHandler filehandler;
+
+        public OrderRepository()
+        {
+            filehandler = new OrderFileHandler();
+        }
+        public ObservableCollection<Order> getAllOrders()
+        {
+            ObservableCollection<Order> loadedOrders = new ObservableCollection<Order>();
+            loadedOrders = filehandler.Load();
+            return loadedOrders;
+        }
+    }
+}
---
+++ Zdravo/Zdravo/ViewModel/OrderViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileHandler;
+using System.Collections.ObjectModel;
+using Zdravo.Model;
+using Service;
+using Model;
+using System.ComponentModel;
+using Repository;
+
+namespace ViewModel
+{
+    internal class OrderViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private ObservableCollection<Order> orders;
+        private Order order;
+        private OrderRepository orderRepository;
+        public ObservableCollection<Order> Orders
+        {
+            get
+            {
+                return orders;
+            }
+            set
+            {
+                if (orders == value)
+                    return;
+                orders = orderRepository.getAllOrders();
+                NotifyPropertyChanged("Orders");
+            }
+        }
+
+        public OrderViewModel()
+        {
+            orderRepository=new OrderRepository();
+            orders = orderRepository.getAllOrders();
+        }
+        public void Refresh()
+        {
+            orderRepository = new OrderRepository();
+            orders = orderRepository.getAllOrders();
+        }
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
